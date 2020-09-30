a6648822fb6ac45110d1b34e5cc3dd018a5cb3eb
KompAc-ua <nsvsergey18@gmail.com>
2020-09-30 13:57:56 +0300

    add map, some flex and fix heroblock padding

--- src/components/AirQuality/AirQuality.js
+++ src/components/AirQuality/AirQuality.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxLanguage from "@mapbox/mapbox-gl-language";
 import "./AirQuality.scss";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic2VyaGlpbm9jaCIsImEiOiJja2ZvZjF0NG0yaHYxMnJudnN3ejBqZW1lIn0.08BxLpUm1LosFbjCw3w_DA";
 
 const AirQuality = () => {
   const mapContainer = useRef(null);
-  const [loacationInfo, setLocationInfo] = useState({
+  const [loacationInfo] = useState({
     lng: 31,
     lat: 48.3,
-    zoom: 5,
+    zoom: 5.2,
   });
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/outdoors-v11",
+      style: "mapbox://styles/mapbox/outdoors-v10",
       center: [loacationInfo.lng, loacationInfo.lat],
       zoom: loacationInfo.zoom,
     });
-  }, []);
+    map.addControl(new MapboxLanguage());
+  }, [loacationInfo.lng, loacationInfo.lat, loacationInfo.zoom]);
   return (
     <section className="air-q container" id="index">
       <h2 className="air-q-title">
